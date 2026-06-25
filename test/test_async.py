52f3784fbe97b2a36f502212684de1a6632e77e3
vutung89 <vutung8988@gmail.com>
2026-06-25 11:20:08 +0700

    update head_byte, crc16_cal (len+load) like mavlinkv2, test async

--- test/test_async.py
+++ test/test_async.py
@@ -5,32 +5,6 @@
     @date 2025-07-29
     @version 2.0
     @details:
-    - Mục đích của tập tin này là để kiểm tra việc gửi 
-    lệnh RC và nhận phản hồi từ bộ điều khiển RC thông qua cổng serial.
-    - Sử dụng thư viện serial_asyncio để thực hiện các thao tác bất đồng 
-    bộ với cổng serial.
-    @usage:
-    - Chạy tập tin này để kết nối tới cổng RC, gửi lệnh và nhận dữ liệu RC channels.
-    - Có thể điều chỉnh các giá trị RC (pitch, roll, throttle, yaw) trong 
-    hàm main để kiểm tra các lệnh khác nhau
-    - Có thể thay đổi TIME_DELAY để điều chỉnh thời gian chờ giữa các lệnh.
-    - Nhấn Ctrl+C để dừng chương trình.
-    @Mode RC_control:
-    - Tay điều khiển SIYI MK15
-        1. Chế độ Auto RC_control pitch, roll, throttle, yaw
-            - Đồng thời: Gạt Switch bên phải sang nấc dưới cùng + nhấn Button A sáng đèn
-        2. Chế độ Manual RC_control pitch, roll, throttle, yaw
-            - Một trong hai, hoặc cả hai: Gạt Switch bên phải sang nấc giữa/ trên cùng / nhấn Button A tắt đèn
-        3. Chế độ Ngắt gửi Sbus_out vào Px4
-            - Nhấn Button B sáng đèn
-        4. Chế độ gửi Sbus_out vào Px4
-            - Nhấn Button B tắt đèn 
-    @note:
-    - Đảm bảo rằng cổng serial được kết nối đúng và thiết bị đã sẵn sàng nhận lệnh.
-    - Lưu ý rằng cổng serial và baudrate cần được cấu hình đúng với thiết bị của bạn
-    - Thời gian chờ giữa các lệnh có thể điều chỉnh thông qua biến TIME_DELAY.
-    - Nếu gặp lỗi kết nối, hãy kiểm tra lại cổng serial và thiết bị.
-    - Đảm bảo rằng thư viện crc đã được cài đặt, mã crc của máy tính nhúng và mạch giống nhau để tính toán CRC16.
 """
 # -----------------------------------------------------------------------------------------------------
 import asyncio
